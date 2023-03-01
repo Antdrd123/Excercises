bcc33ff59ef0132b2c7b417081ea82b491926e7d
antdrd123 <antoniodrdic7@gmail.com>
2023-03-01 16:57:48 +0100

    Simplified with methods.

--- Tic_tac_toe_check/Program.cs
+++ Tic_tac_toe_check/Program.cs
@@ -12,61 +12,95 @@ namespace Tic_tac_toe_check
         {
             string[,] board =
             {
-                {"X", "O", "O" },
+                {"X", "O", "X" },
                 {"O", "O", "X" },
-                {"O", "X", "X" }
+                {"O", "X", "O" }
             };
-            if(TicTacToe.Checker(board) == false)
+            if (TicTacToe.Checker(board) == false)
             {
                 Console.WriteLine("It is a draw!");
             }
             Console.ReadKey();
         }
-
     }
     public class TicTacToe
     {
         public static bool Checker(string[,] board)
         {
-            int bint = 0;
-            int mint = 0;
-            bool mool = false;
-            for (int i = 0; i < board.GetLength(0); i++)
+            /*bool check = false;
+            if(CheckHorizontalRows(board) == true)
             {
-                for (int j = 0; j < board.GetLength(1); j++)
-                {
-                    if (board[i, j] == "O")
-                    {
-                        bint++;
-                        if (bint == 3)
-                        {
-                            Console.WriteLine("Player O wins!!");
-                            mool = true;
-                            return true;
-                        }
-                    }
-                    else if (board[i, j] == "X")
-                    {
-                        mint++;
-                        if (mint == 3)
-                        {
-                            Console.WriteLine("Player X wins!!");
-                            mool = true;
-                            return true;
-                        }
-                    }
-                }
-                if(mool == true)
-                {
-                    break;
-                }
-                bint = 0;
-                mint = 0;
+                check = true;
+                return true;
             }
-            bint = 0;
-            mint = 0;
-            if (mool == false)
+            if(check == false)
             {
+                if(CheckVerticalRows(board) == true)
+                {
+                    check = true;
+                    return true;
+                }
+                if(check == false)
+                {
+                    if (CheckDiagonalRows(board) == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+            */
+            
+            if (CheckHorizontalRows(board) == false)
+            {
+                if(CheckVerticalRows(board) == false);
+                {
+                    if(CheckDiagonalRows(board) == false)
+                    {
+                        return false;
+                    }
+                }
+               
+            }
+            return true;
+        }
+
+            public static bool CheckHorizontalRows(string[,] board1)
+            {
+                int bint = 0;
+                int mint = 0;
+                for (int i = 0; i < board1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < board1.GetLength(1); j++)
+                    {
+                        if (board1[i, j] == "O")
+                        {
+                            bint++;
+                            if (bint == 3)
+                            {
+                                Console.WriteLine("Player O wins!!");
+                                return true;
+                            }
+                        }
+                        else if (board1[i, j] == "X")
+                        {
+                            mint++;
+                            if (mint == 3)
+                            {
+                                Console.WriteLine("Player X wins!!");
+                                return true;
+                            }
+                        }
+                    }
+                    bint = 0;
+                    mint = 0;
+                }
+                return false;
+            }
+            public static bool CheckVerticalRows(string[,] board)
+            {
+                int bint = 0;
+                int mint = 0;
                 for (int i = 0; i < board.GetLength(1); i++)
                 {
                     for (int j = 0; j < board.GetLength(0); j++)
@@ -77,7 +111,6 @@ namespace Tic_tac_toe_check
                             if (bint == 3)
                             {
                                 Console.WriteLine("Player O wins!!");
-                                mool = true;
                                 return true;
                             }
                         }
@@ -87,7 +120,6 @@ namespace Tic_tac_toe_check
                             if (mint == 3)
                             {
                                 Console.WriteLine("Player X wins!!");
-                                mool = true;
                                 return true;
                             }
                         }
@@ -95,9 +127,13 @@ namespace Tic_tac_toe_check
                     bint = 0;
                     mint = 0;
                 }
+                return false;
             }
-            if (mool == false)
+            public static bool CheckDiagonalRows(string[,] board)
             {
+                int mint = 0;
+                int bint = 0;
+                bool mool = false;
                 for (int i = 0; i < board.GetLength(0); i++)
                 {
                     if (board[i, i] == "O")
@@ -121,43 +157,44 @@ namespace Tic_tac_toe_check
                         }
                     }
                 }
-            }
-            bint = 0;
-            mint = 0;
-            if (mool == false)
-            {
-                for (int i = 0; i < board.GetLength(0); i++)
+                bint = 0;
+                mint = 0;
+                if (mool == false)
                 {
-                    for (int j = board.GetLength(1) - 1; j >= 0; j--)
+                    for (int i = 0; i < board.GetLength(0); i++)
                     {
-                        if (i + j == board.GetLength(1) - 1)
+                        for (int j = board.GetLength(1) - 1; j >= 0; j--)
                         {
-                            if (board[i, j] == "O")
+                            if (i + j == board.GetLength(1) - 1)
                             {
-                                bint++;
-                                if (bint == 3)
+                                if (board[i, j] == "O")
                                 {
-                                    Console.WriteLine("Player O wins!!");
-                                    mool = true;
-                                    return true;
+                                    bint++;
+                                    if (bint == 3)
+                                    {
+                                        Console.WriteLine("Player O wins!!");
+                                        mool = true;
+                                        return true;
+                                    }
                                 }
-                            }
-                            else if (board[i, j] == "X")
-                            {
-                                mint++;
-                                if (mint == 3)
+                                else if (board[i, j] == "X")
                                 {
-                                    Console.WriteLine("Player X wins!!");
-                                    mool = true;
-                                    return true;
+                                    mint++;
+                                    if (mint == 3)
+                                    {
+                                        Console.WriteLine("Player X wins!!");
+                                        mool = true;
+                                        return true;
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                return false;
             }
-            return false;
-            }
-        }
+        
     }
+}
+   
 
